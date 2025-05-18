d94f1933618df102806e4739c7f2e5f6312013c8
RedWebAppWriting <reddy.336@osu.edu>
2025-05-18 17:06:28 -0400

    Update Map.java

--- src/Map.java
+++ src/Map.java
@@ -1,1 +1,346 @@
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import components.map.Map;
+import components.map.Map1L;
+import components.map.MapSecondary;
+
+/**
+ * {@code Map} represented as a hash table using {@code Map}s for the buckets,
+ * with implementations of primary methods.
+ *
+ * @param <K>
+ *            type of {@code Map} domain (key) entries
+ * @param <V>
+ *            type of {@code Map} range (associated value) entries
+ * @convention <pre>
+ * |$this.hashTable| > 0  and
+ * for all i: integer, pf: PARTIAL_FUNCTION, x: K
+ *     where (0 <= i  and  i < |$this.hashTable|  and
+ *            <pf> = $this.hashTable[i, i+1)  and
+ *            x is in DOMAIN(pf))
+ *   ([computed result of x.hashCode()] mod |$this.hashTable| = i))  and
+ * for all i: integer
+ *     where (0 <= i  and  i < |$this.hashTable|)
+ *   ([entry at position i in $this.hashTable is not null])  and
+ * $this.size = sum i: integer, pf: PARTIAL_FUNCTION
+ *     where (0 <= i  and  i < |$this.hashTable|  and
+ *            <pf> = $this.hashTable[i, i+1))
+ *   (|pf|)
+ * </pre>
+ * @correspondence <pre>
+ * this = union i: integer, pf: PARTIAL_FUNCTION
+ *            where (0 <= i  and  i < |$this.hashTable|  and
+ *                   <pf> = $this.hashTable[i, i+1))
+ *          (pf)
+ * </pre>
+ *
+ * @authors Sam Fess, Akhilesh Reddy
+ *
+ */
+public class Map4<K, V> extends MapSecondary<K, V> {
+
+    /*
+     * Private members --------------------------------------------------------
+     */
+
+    /**
+     * Default size of hash table.
+     */
+    private static final int DEFAULT_HASH_TABLE_SIZE = 101;
+
+    /**
+     * Buckets for hashing.
+     */
+    private Map<K, V>[] hashTable;
+
+    /**
+     * Total size of abstract {@code this}.
+     */
+    private int size;
+
+    /**
+     * Computes {@code a} mod {@code b} as % should have been defined to work.
+     *
+     * @param a
+     *            the number being reduced
+     * @param b
+     *            the modulus
+     * @return the result of a mod b, which satisfies 0 <= {@code mod} < b
+     * @requires b > 0
+     * @ensures <pre>
+     * 0 <= mod  and  mod < b  and
+     * there exists k: integer (a = k * b + mod)
+     * </pre>
+     */
+    private static int mod(int a, int b) {
+        assert b > 0 : "Violation of: b > 0";
+
+        int ans = a % b;
+        if (ans < 0) {
+            ans += b;
+        }
+        return ans;
+    }
+
+    /**
+     * Creator of initial representation.
+     *
+     * @param hashTableSize
+     *            the size of the hash table
+     * @requires hashTableSize > 0
+     * @ensures <pre>
+     * |$this.hashTable| = hashTableSize  and
+     * for all i: integer
+     *     where (0 <= i  and  i < |$this.hashTable|)
+     *   ($this.hashTable[i, i+1) = <{}>)  and
+     * $this.size = 0
+     * </pre>
+     */
+    @SuppressWarnings("unchecked")
+    private void createNewRep(int hashTableSize) {
+        /*
+         * With "new Map<K, V>[...]" in place of "new Map[...]" it does not
+         * compile; as shown, it results in a warning about an unchecked
+         * conversion, though it cannot fail.
+         */
+        this.size = 0;
+        this.hashTable = new Map[hashTableSize];
+        int i = 0;
+        while (i < this.hashTable.length) {
+
+            this.hashTable[i] = new Map1L<K, V>();
+            i++;
+        }
+
+    }
+
+    /*
+     * Constructors -----------------------------------------------------------
+     */
+
+    /**
+     * No-argument constructor.
+     */
+    public Map4() {
+
+        this.createNewRep(DEFAULT_HASH_TABLE_SIZE);
+
+    }
+
+    /**
+     * Constructor resulting in a hash table of size {@code hashTableSize}.
+     *
+     * @param hashTableSize
+     *            size of hash table
+     * @requires hashTableSize > 0
+     * @ensures this = {}
+     */
+    public Map4(int hashTableSize) {
+
+        this.createNewRep(hashTableSize);
+
+    }
+
+    /*
+     * Standard methods -------------------------------------------------------
+     */
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public final Map<K, V> newInstance() {
+        try {
+            return this.getClass().getConstructor().newInstance();
+        } catch (ReflectiveOperationException e) {
+            throw new AssertionError(
+                    "Cannot construct object of type " + this.getClass());
+        }
+    }
+
+    @Override
+    public final void clear() {
+        this.createNewRep(DEFAULT_HASH_TABLE_SIZE);
+    }
+
+    @Override
+    public final void transferFrom(Map<K, V> source) {
+        assert source != null : "Violation of: source is not null";
+        assert source != this : "Violation of: source is not this";
+        assert source instanceof Map4<?, ?> : ""
+                + "Violation of: source is of dynamic type Map4<?,?>";
+        /*
+         * This cast cannot fail since the assert above would have stopped
+         * execution in that case: source must be of dynamic type Map4<?,?>, and
+         * the ?,? must be K,V or the call would not have compiled.
+         */
+        Map4<K, V> localSource = (Map4<K, V>) source;
+        this.hashTable = localSource.hashTable;
+        this.size = localSource.size;
+        localSource.createNewRep(DEFAULT_HASH_TABLE_SIZE);
+    }
+
+    /*
+     * Kernel methods ---------------------------------------------------------
+     */
+
+    @Override
+    public final void add(K key, V value) {
+        assert key != null : "Violation of: key is not null";
+        assert value != null : "Violation of: value is not null";
+        assert !this.hasKey(key) : "Violation of: key is not in DOMAIN(this)";
+
+        //get hashValue in order to determine the correct bin to store value
+        int hashValue = key.hashCode();
+        //increment size accordingly
+        this.size++;
+        int bin = mod(hashValue, this.hashTable.length);
+
+        //Check to make sure that the bin does not already contain key
+        if (!this.hashTable[bin].hasKey(key)) {
+            this.hashTable[bin].add(key, value);
+        }
+
+    }
+
+    @Override
+    public final Pair<K, V> remove(K key) {
+        assert key != null : "Violation of: key is not null";
+        assert this.hasKey(key) : "Violation of: key is in DOMAIN(this)";
+
+        int hashValue = key.hashCode();
+        //get hashValue in order to get to the correct bin and then remove the
+        //key, adjust size accordingly
+        int bin = mod(hashValue, this.hashTable.length);
+        Map.Pair<K, V> removal = this.hashTable[bin].remove(key);
+        this.size--;
+
+        return removal;
+    }
+
+    @Override
+    public final Pair<K, V> removeAny() {
+        assert this.size() > 0 : "Violation of: this /= empty_set";
+
+        //set iterator to 0 and flag to false
+        int binerator = 0;
+        boolean fullBin = false;
+
+        while (this.hashTable[binerator].size() == 0) {
+
+            binerator++;
+
+        }
+
+        Map.Pair<K, V> removal = this.hashTable[binerator].removeAny();
+        this.size--;
+
+        return removal;
+    }
+
+    @Override
+    public final V value(K key) {
+        assert key != null : "Violation of: key is not null";
+        assert this.hasKey(key) : "Violation of: key is in DOMAIN(this)";
+
+        //generate the bin number from a key,
+        //take out the corresponding pair from the bin
+        //report the value
+        //place values back into bin
+        int hashValue = key.hashCode();
+        int bin = mod(hashValue, this.hashTable.length);
+        Map.Pair<K, V> removal = this.hashTable[bin].remove(key);
+        V worth = removal.value();
+        this.hashTable[bin].add(key, worth);
+        return worth;
+    }
+
+    @Override
+    public final boolean hasKey(K key) {
+        assert key != null : "Violation of: key is not null";
+
+        //given the key find the corresponding bin it would be kept into
+        //through the hashing formula, then check that bin to see
+        //if it is located there
+        boolean checker = false;
+        int hashValue = key.hashCode();
+        int bin = mod(hashValue, this.hashTable.length);
+        checker = this.hashTable[bin].hasKey(key);
+        return checker;
+
+    }
+
+    @Override
+    public final int size() {
+
+        return this.size;
+    }
+
+    @Override
+    public final Iterator<Pair<K, V>> iterator() {
+        return new Map4Iterator();
+    }
+
+    /**
+     * Implementation of {@code Iterator} interface for {@code Map4}.
+     */
+    private final class Map4Iterator implements Iterator<Pair<K, V>> {
+
+        /**
+         * Number of elements seen already (i.e., |~this.seen|).
+         */
+        private int numberSeen;
+
+        /**
+         * Bucket from which current bucket iterator comes.
+         */
+        private int currentBucket;
+
+        /**
+         * Bucket iterator from which next element will come.
+         */
+        private Iterator<Pair<K, V>> bucketIterator;
+
+        /**
+         * No-argument constructor.
+         */
+        Map4Iterator() {
+            this.numberSeen = 0;
+            this.currentBucket = 0;
+            this.bucketIterator = Map4.this.hashTable[0].iterator();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return this.numberSeen < Map4.this.size;
+        }
+
+        @Override
+        public Pair<K, V> next() {
+            assert this.hasNext() : "Violation of: ~this.unseen /= <>";
+            if (!this.hasNext()) {
+                /*
+                 * Exception is supposed to be thrown in this case, but
+                 * assertion-checking enabled it cannot happen because of assert
+                 * above.
+                 */
+                throw new NoSuchElementException();
+            }
+            this.numberSeen++;
+            while (!this.bucketIterator.hasNext()) {
+                this.currentBucket++;
+                this.bucketIterator = Map4.this.hashTable[this.currentBucket]
+                        .iterator();
+            }
+            return this.bucketIterator.next();
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException(
+                    "remove operation not supported");
+        }
+
+    }
+
+}
